26d5a71343305f1983e9c66cca5158989379d340
Imtiyaz Miah <imtiyazmiah@gmail.com>
2018-05-14 23:00:50 +0100

    completed code

--- com.py
+++ com.py
@@ -1,5 +1,5 @@
-#Calculates the centre of mass of multiple objects on a 2d plane
-
+#Calculates the centre of mass of a composite shape on a 2d plane
+#!python3
 objectsMass = []    
 objectsXPos = []
 objectsYPos = []
@@ -17,4 +17,28 @@ def objects():
             x = True
         else:
             x = False
+            
+    i = 0
+    totalWeight = 0
+    while i < n:
+        totalWeight += objectsMass[i]
+        i+=1
+    
+    #xcomponent of the com
+    i = 0
+    xWeights = 0
+    while i < n:
+        xWeights += objectsMass[i] * objectsXPos[i]
+        i+=1
+    xPos = xWeights / totalWeight
+    #ycomponent of the com
+    i = 0
+    yWeights = 0
+    while i < n:
+        yWeights += objectsMass[i] * objectsYPos[i]
+        i+=1
+    yPos = yWeights / totalWeight
+
+    print("The x component of the centre of mass of the composite object is: " + str(xPos) + " and the y component is: " + str(yPos))
 objects()
+
